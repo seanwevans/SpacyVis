4a703c218cef0960cd313e3382174956036cd895
Sean Evans <sean.w.evans@gmail.com>
2025-06-04 09:37:40 -0400

    Update Multer to v2 and handle upload errors

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const upload = multer({ storage });
 
 function getRandomColor() {
     const letters = '0123456789ABCDEF';
@@ -39,43 +39,54 @@ app.get('/', (req, res) => {
 	});
 });
 
-app.post('/upload', upload.single('xmlfile'), (req, res) => {
+app.post('/upload', (req, res) => {
     const startTime = Date.now();
-    const xmlData = req.file.buffer.toString();
-
-    xml2js.parseString(xmlData, (err, result) => {
-        if (err) {
-            res.status(500).send('Unable to parse XML');
-            return;
+    upload.single('xmlfile')(req, res, (err) => {
+        if (err instanceof multer.MulterError) {
+            return res.status(400).send(err.message);
+        } else if (err) {
+            return res.status(500).send('An unknown error occurred when uploading.');
         }
 
-        const words = result.document.word || [];
-        const wordsData = words.map(wordElem => {
-            const wordInfo = { value: wordElem.$.value }; // Get attributes
-            (wordElem.method || []).forEach(methodElem => {
-                wordInfo[methodElem.$.value] = methodElem._;
+        if (!req.file) {
+            return res.status(400).send('No file uploaded');
+        }
+
+        const xmlData = req.file.buffer.toString();
+        xml2js.parseString(xmlData, (parseErr, result) => {
+            if (parseErr) {
+                res.status(500).send('Unable to parse XML');
+                return;
+            }
+
+            const words = result.document.word || [];
+            const wordsData = words.map(wordElem => {
+                const wordInfo = { value: wordElem.$.value }; // Get attributes
+                (wordElem.method || []).forEach(methodElem => {
+                    wordInfo[methodElem.$.value] = methodElem._;
+                });
+                return wordInfo;
             });
-            return wordInfo;
+
+            const uniqueEntityTypes = [...new Set(wordsData.map(word => word.ent_type_))].filter(Boolean);
+            const entityTypeColorMap = {};
+            const entityTypeTextColorMap = {};
+            uniqueEntityTypes.forEach(type => {
+                const bgColor = getRandomColor();
+                entityTypeColorMap[type] = bgColor;
+                entityTypeTextColorMap[type] = isColorDark(bgColor) ? '#FFFFFF' : '#000000';
+            });
+
+            res.render('index', {
+                wordsData: wordsData,
+                uniqueEntityTypes: uniqueEntityTypes,
+                entityTypeColorMap: entityTypeColorMap,
+                entityTypeTextColorMap: entityTypeTextColorMap
+            });
+
+            const endTime = Date.now();
+            console.log(`File '${req.file.originalname}' processed in ${endTime-startTime}ms`);
         });
-		
-		const uniqueEntityTypes = [...new Set(wordsData.map(word => word.ent_type_))].filter(Boolean);
-		const entityTypeColorMap = {};
-		const entityTypeTextColorMap = {};
-		uniqueEntityTypes.forEach(type => {
-			const bgColor = getRandomColor();
-			entityTypeColorMap[type] = bgColor;
-			entityTypeTextColorMap[type] = isColorDark(bgColor) ? '#FFFFFF' : '#000000';
-		});
-
-		res.render('index', { 
-			wordsData: wordsData, 
-			uniqueEntityTypes: uniqueEntityTypes, 
-			entityTypeColorMap: entityTypeColorMap,
-			entityTypeTextColorMap: entityTypeTextColorMap
-		});
-
-        const endTime = Date.now();
-        console.log(`File '${req.file.originalname}' processed in ${endTime-startTime}ms`);
     });
 });
 
